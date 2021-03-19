b1f3a47f54521b87ec8e3724f1419289c03e9965
ivan_berko <berkoivan@gmail.com>
2021-03-19 11:30:23 +0200

    add Context

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,40 @@
+import { useState, useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+
+// Context
+import Context from '../../context'
+
+// Components
 import MainPage from '../../pages/MainView';
 import CartPage from '../../pages/CartView';
 import EditCardPage from '../../pages/EditView';
 import CreateCardPage from '../../pages/CreateView';
+
+// Services
+import { fetchProducts } from '../../services/apiServices';
+
+// Styles
 import './App.module.css';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts().then(data => setProducts(data));
+  }, []);
+
   return (
-    <main className='app'>
-      <Switch>
-        <Route path='/' exact component={MainPage} />
-        <Route path='/cart' exact component={CartPage} />
-        <Route path='/edit' exact component={EditCardPage} />
-        <Route path='/create' exact component={CreateCardPage} />
-        <Redirect to='/' />
-      </Switch>
-    </main>
+    <Context.Provider value={{products, setProducts}}>
+      <main className='app'>
+        <Switch>
+          <Route path='/' exact component={MainPage} />
+          <Route path='/cart' component={CartPage} />
+          <Route path='/edit' component={EditCardPage} />
+          <Route path='/create' component={CreateCardPage} />
+          <Redirect to='/' />
+        </Switch>
+      </main>
+    </Context.Provider>
   );
 };
 
