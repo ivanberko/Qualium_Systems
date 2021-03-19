5f0f349d1a988af8e10d1c529eb1641afa61bbdc
ivan_berko <berkoivan@gmail.com>
2021-03-19 11:34:28 +0200

    add a Button "create" and a list of cards to the Main page

--- src/pages/MainView/index.js
+++ src/pages/MainView/index.js
@@ -1,7 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import CardList from '../../components/ListCards';
+import Button from '../../components/Button';
+
 const MainPage = () => {
   return (
-    <h1>Main Page</h1>
-  )
-}
+    <section>
+      <h1>Main Page </h1>
+      <NavLink to='/create'>
+        <Button label={'Create'}/>
+      </NavLink>
+      <CardList />
+    </section>
+  );
+};
 
 export default MainPage;
