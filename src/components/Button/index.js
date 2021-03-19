0994073459d5733494d433ae6735dd1a70649986
ivan_berko <berkoivan@gmail.com>
2021-03-19 11:29:07 +0200

    add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import { button } from './Button.module.css';
+
+const Button = ({label, handleClick, isActive}) => {
+  return (
+    <button className={button} type='button' onClick={handleClick} disabled={isActive}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
